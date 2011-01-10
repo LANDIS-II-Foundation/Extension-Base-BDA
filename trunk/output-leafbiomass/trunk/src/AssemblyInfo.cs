21a5f9276762cf446848aeec651a1263dce1e024
Rob <rmscheller@gmail.com>
2011-01-10 17:10:16 +0000

    Removed output-leaf-biomass.  Needed to be reconfigured.
    git-svn-id: http://Marc-PC/svn/Full@28 85f15e0e-5137-b144-a863-98bd542365a3

--- trunk/output-leafbiomass/trunk/src/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyDescription("Output Biomass Plug-in")]
-[assembly: AssemblyProduct("Landis-II")]
-[assembly: AssemblyCompany("UW-Madison, Forest Landscape Ecology Lab")]
-[assembly: AssemblyCopyright("Copyright 2005 University of Wisconsin")]
