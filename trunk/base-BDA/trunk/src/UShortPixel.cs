14bb42db41ebf06e0ed719d86054cd42498276af
Rob <rmscheller@gmail.com>
2011-01-10 23:06:57 +0000

    Added Base BDA to the repository.
    git-svn-id: http://Marc-PC/svn/Full@35 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ trunk/base-BDA/trunk/src/UShortPixel.cs
@@ -0,0 +1,17 @@
+//  Copyright 2005-2010 Portland State University, University of Wisconsin
+//  Authors:   , Robert M. Scheller, James B. Domingo
+
+using Landis.SpatialModeling;
+
+namespace Landis.Extension.BaseBDA
+{
+    public class UShortPixel : Pixel
+    {
+        public Band<ushort> MapCode  = "The numeric code for each raster cell";
+
+        public UShortPixel()
+        {
+            SetBands(MapCode);
+        }
+    }
+}
