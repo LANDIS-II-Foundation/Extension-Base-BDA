aaf4544911f52ad1e1b07fd6f797e4b0ca9cb7e9
Rob <rmscheller@gmail.com>
2011-01-13 19:02:41 +0000

    Minor cleanup.
    git-svn-id: http://Marc-PC/svn/Full@62 85f15e0e-5137-b144-a863-98bd542365a3

--- trunk/leaf-biomass-cohort/trunk/src/DisturbanceEventHandler.cs
+++
@@ -1,8 +0,0 @@
-namespace Landis.Library.LeafBiomassCohorts
-{
-    /// <summary>
-    /// Represents a method that handles disturbance events.
-    /// </summary>
-    public delegate void DisturbanceEventHandler(object               sender,
-                                                 DisturbanceEventArgs eventArgs);
-}
