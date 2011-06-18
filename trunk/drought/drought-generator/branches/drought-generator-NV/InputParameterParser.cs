900c1c9cbb307e341adac4be035a951bd2b4292e
Rob <rmscheller@gmail.com>
2011-06-18 18:38:11 +0000

    Created branch for drought in Nevada project.
    git-svn-id: http://Marc-PC/svn/Full@276 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ trunk/drought/drought-generator/branches/drought-generator-NV/InputParameterParser.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Collections.Generic;
+
+using Edu.Wisc.Forest.Flel.Util;
+
+namespace Landis.Extension.DroughtGenerator
+{
+    /// <summary>
+    /// A parser that reads the plug-in's parameters from text input.
+    /// </summary>
+    class InputParametersParser
+        : TextParser<IInputParameters>
+    {
+        //---------------------------------------------------------------------
+        //public InputParameterParser()
+        //{
+         //   RegisterForInputValues();
+        //}
+        //---------------------------------------------------------------------
+        protected override IInputParameters Parse()
+        {
+            InputVar<string> landisData = new InputVar<string>("LandisData");
+            ReadVar(landisData);
+            if (landisData.Value.Actual != PlugIn.ExtensionName)
+                throw new InputValueException(landisData.Value.String, "The value is not \"{0}\"", PlugIn.ExtensionName);
+
+            InputParameters parameters = new InputParameters();
+
+            InputVar<int> timestep = new InputVar<int>("Timestep");
+            ReadVar(timestep);
+            parameters.Timestep = timestep.Value;
+
+            InputVar<double> mu = new InputVar<double>("Mu");
+            ReadVar(mu);
+            parameters.Mu = mu.Value;
+
+            InputVar<double> sigma = new InputVar<double>("Sigma");
+            ReadVar(sigma);
+            parameters.Sigma = sigma.Value;
+
+            InputVar<string> mapNames = new InputVar<string>("MapName");
+            ReadVar(mapNames);
+            parameters.MapNamesTemplate = mapNames.Value;
+
+            InputVar<string> logFile = new InputVar<string>("LogFile");
+            ReadVar(logFile);
+            parameters.LogFileName = logFile.Value;
+
+            return parameters;
+
+        }
+
+    }
+}
---
+++ trunk/drought/drought-generator/branches/drought-generator-NV/UShortPixel.cs
@@ -0,0 +1,14 @@
+﻿using Landis.SpatialModeling;
+
+namespace Landis.Extension.DroughtGenerator
+{
+    public class UShortPixel : Pixel
+    {
+        public Band<ushort> MapCode = "The numeric code for each raster cell";
+
+        public UShortPixel()
+        {
+            SetBands(MapCode);
+        }
+    }
+}
