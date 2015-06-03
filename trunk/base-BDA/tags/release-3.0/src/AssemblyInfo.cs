565ef714824db0b8bda8763ccdb3cd2966be0ca9
Brian <brmirand@gmail.com>
2015-06-03 14:46:21 +0000

    Tag release-3.0
    git-svn-id: http://Marc-PC/svn/Full@3734 85f15e0e-5137-b144-a863-98bd542365a3

---
+++ trunk/base-BDA/tags/release-3.0/src/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Base BDA Extension")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("3.0.*")]
